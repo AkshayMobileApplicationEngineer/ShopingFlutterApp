ff7fb35bebbe6b4a9051e9f1460080b10db565eb
AkshayMobileApplicationEngineer <akshaykumarprajapati@hotmail.com>
2024-10-11 14:17:16 +0530

    Style : main_screen.dart UI done

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -8,72 +8,81 @@ class MainScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "data",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome to MyApp", // Title
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Object this is just is the object",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
+                SizedBox(height: 20),
+                Text(
+                  "This is just an example of the main screen.", // Content text
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center, // Correctly used here
                 ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Image.network(
-                'https://quizparlour.in/assets/img/logo.png',
-                height: 100,
-                width: 100,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "More Fun",
-                  style: TextStyle(fontSize: 18),
+                SizedBox(height: 20),
+                Image.network(
+                  'https://quizparlour.in/assets/img/logo.png',
+                  height: 100,
+                  width: 100,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Icon(Icons.error, color: Colors.red, size: 100);
+                  },
                 ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add your action here
+                  },
+                  child: Text(
+                    "More Fun",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Spacer(),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Created by Akshay ",
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                      ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "Version 1",
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                  ),
-                ],
-              ),
-            ],
+                SizedBox(height: 20), // Space between button and footer
+                Spacer(),
+                // Footer
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Created by Akshay ",
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Version 1",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
