9846e7edcb580aa6ddb9389c7e80d0a49b59b61d
AkshayMobileApplicationEngineer <akshaykumarprajapati@hotmail.com>
2024-10-11 20:35:18 +0530

    AppBar: title name is set

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MyAmazonApp());
 }
 
-class MyAmazonApp {
-  const MyAmazonApp();
+class MyAmazonApp extends StatelessWidget {
+  const MyAmazonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Add this line
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Amazon",style: TextStyle(
+            fontSize: 28
+          ),
+          ),
+        ),
+      ),
+    );
+  }
 }
