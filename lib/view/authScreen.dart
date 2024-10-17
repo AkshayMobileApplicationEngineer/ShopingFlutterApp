11eafcf36061449e40b637a01643b2f68bff5192
AkshayMobileApplicationEngineer <akshaykumarprajapati@hotmail.com>
2024-10-17 23:14:56 +0530

    create sign in screenLog

--- lib/view/authScreen.dart
+++ lib/view/authScreen.dart
@@ -25,115 +25,121 @@ class _AuthScreenState extends State<AuthScreen> {
         ),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                "Sign in with email and Password",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height - 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   const Text(
-                    "Sign In",
+                    "Sign in with email and Password",
                     style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Navigate to Forgot Password screen
+                          print("Navigate to Forgot Password");
+                        },
+                        child: const Text("Forgot Password"),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    textAlign: TextAlign.center,
+                    obscureText: !showPassword,
+                  ),
+                  CheckboxListTile(
+                    value: showPassword,
+                    onChanged: (value) {
+                      setState(() {
+                        showPassword = value!;
+                      });
+                    },
+                    title: const Text("Show password"),
+                  ),
+                  CheckboxListTile(
+                    value: keepSignedIn,
+                    onChanged: (value) {
+                      setState(() {
+                        keepSignedIn = value!;
+                      });
+                    },
+                    title: const Text("Keep Sign in"),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Add your sign-in logic here
+                      print("Sign In with email: ${emailController.text}");
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.amber,
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Center(child: const Text("Sign In")),
+                    ),
+                  ),
+                  const Divider(thickness: 1),
+                  Center(child: const Text("New to UrbanBasket Shopping App?")),
+                  const SizedBox(height: 10),
+                  InkWell(
+                    onTap: () {
+                      // Navigate to account creation screen
+                      print("Create an UrbanBasket Account");
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Center(child: const Text("Create an UrbanBasket Account")),
+                    ),
+                  ),
+                  const SizedBox(height: 80), // Add some space before the footer
                   TextButton(
                     onPressed: () {
-                      // Navigate to Forgot Password screen
+                      // Navigate to privacy notes or show them
+                      print("Conditions of the Privacy Notes");
                     },
-                    child: const Text("Forgot Password"),
+                    child: const Text("Conditions of the Privacy Notes"),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                textAlign: TextAlign.center,
-                obscureText: !showPassword,
-              ),
-              CheckboxListTile(
-                value: showPassword,
-                onChanged: (value) {
-                  setState(() {
-                    showPassword = value!;
-                  });
-                },
-                title: const Text("Show password"),
-              ),
-              CheckboxListTile(
-                value: keepSignedIn,
-                onChanged: (value) {
-                  setState(() {
-                    keepSignedIn = value!;
-                  });
-                },
-                title: const Text("Keep Sign in"),
-              ),
-              GestureDetector(
-                onTap: () {
-                  // Add your sign-in logic here
-                  print("Sign In with email: ${emailController.text}");
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.amber,
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: Center(child: const Text("Sign In")),
-                ),
-              ),
-              const Divider(thickness: 1),
-              Center(child: const Text("New to UrbanBasket Shopping App?")),
-              const SizedBox(height: 10),
-              InkWell(
-                onTap: () {
-                  // Navigate to account creation screen
-                  print("Create an UrbanBasket Account");
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: Center(child: const Text("Create an UrbanBasket Account")),
-                ),
-              ),
-              Spacer(),
-              TextButton(
-                onPressed: () {
-
-                },
-                child: const Text("Conditions of the Privacy Notes"),
-              )
-
-            ],
+            ),
           ),
         ),
       ),
